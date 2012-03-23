855476db6071e53ac1bf4d07278d3f9355d831a1
Martok <martok@martoks-place.de>
2012-03-23 05:39:19 +0100

    add: direct pixel acces for surface; change to pf32bit to make blitting faster

--- engine/uSurface.pas
+++ engine/uSurface.pas
@@ -7,10 +7,14 @@ uses Windows, Types, SysUtils, Graphics, uSprite;
 type
   TSurface = class(TBitmap)
   private
+    function GetData(X, Y: Cardinal): PCardinal;
+    function GetPixel(X, Y: integer): TColor;
+    procedure SetPixel(X, Y: integer; const Value: TColor);
   public
     constructor Create; override;
     procedure Clear(Color: TColor);
     procedure Blit(X, Y: integer; Graphic: TSprite);
+    property Pixel[X, Y: integer]: TColor read GetPixel write SetPixel;
   end;
 
 implementation
@@ -32,9 +36,24 @@ end;
 constructor TSurface.Create;
 begin
   inherited;
-  PixelFormat:= pf24bit;
+  PixelFormat:= pf32bit;
   Canvas.Font.Name:= 'Fixedsys';
   Canvas.Font.Size:= 16;
+end;
+
+function TSurface.GetPixel(X, Y: integer): TColor;
+begin
+  Result:= GetData(X, Y)^;
+end;
+
+procedure TSurface.SetPixel(X, Y: integer; const Value: TColor);
+begin
+  GetData(X, Y)^:= Value;
+end;
+
+function TSurface.GetData(X, Y: Cardinal): PCardinal;
+begin
+  Result:= PCardinal(Cardinal(Scanline[Y]) + X * sizeof(Cardinal));
 end;
 
 end.
